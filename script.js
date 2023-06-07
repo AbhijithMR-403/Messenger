80378cacca85a2d9b28dbae9abc51609c4466045
Abhijith M R <abhijithmr581@gmail.com>
2023-06-07 18:50:14 +0530

    Completed the validation

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-var w = window.innerWidth;
-var hangOut=document.getElementById("hang-out")
-console.log(w)
-window.addEventListener("resize",console.log(w));
-if(w>993)
-{
-    hangOut.classList.add("d-inline-block");
+
+const signInForm = document.getElementById("signin-form")
+
+signInForm.addEventListener("submit", valid);
+function valid() {
+
+    let password = document.getElementById("password").value;
+    let userInfo = document.getElementById("userInfo").value;
+    let userError = document.getElementById("user-error")
+    let passwordError = document.getElementById("password-error")
+    if(userInfo=="")
+    {
+        userError.innerHTML="number or email please"
+    }else if(!isNaN(userInfo))
+    {
+        if(userInfo<Math.pow(10,9))
+        userError.innerHTML="Enter 10 digit of numbers"
+    }
+    else if(!userInfo.includes("@"))
+    {
+            userError.innerHTML="Enter a valid email"
+    }
+    else{
+        userError.innerHTML=""
+    }
+
+    if(password=="")
+    {
+        passwordError.innerHTML="Why can't you type the password"
+        
+    }else if (password.length<6) {
+        passwordError.innerHTML="type more..."
+    }
+    else{
+        passwordError.innerHTML="";
+    }
 }
